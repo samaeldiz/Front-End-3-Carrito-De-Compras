314007457a36e002c9ba8c3bb159754e43a5c7ea
samaeldiz <samaeldiz@gmail.com>
2022-09-02 13:51:00 -0500

    first commit

---
+++ src/components/Listado.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import Item from './Item.js'
+const data = require('./data.json');
+
+// Debemos importar el listado de objetos del archivo JSON para usarlos como array a la hora de crear cada Item.
+// El componente Listado es el padre de:
+// - varios Item
+// ESTADO: Listado no necesita manejar un estado.
+// MÉTODOS: Listado no requiere de métodos.
+// PROPS: Listado recibe el método para aumentar el estado de App y se lo pasa a cada uno de sus hijos.
+
+export default function Listado({agregarElementos}) {
+  return (
+    <div className='container'>
+      {data.map((item)=>{
+        return <Item key={item.id} stock={item.stock} img={item.producto.img}  nombre={item.producto.nombre} descripcion={item.producto.descripcion} agregarElemento={agregarElementos}/>;
+      })}
+    </div>
+  )
+}
